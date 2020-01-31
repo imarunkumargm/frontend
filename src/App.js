62d26961faf3be202b91e19b25e1262e8ff2cef8
Arun Kumar <arun.kumar@appoets.com>
2020-01-31 19:05:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: ''
+    }
+    this.changeName = this.changeName.bind(this);
+    this.changeEmail = this.changeEmail.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+  changeName(event) {
+    this.setState({name: event.target.value});
+  }
+  changeEmail(event) {
+    this.setState({email: event.target.value});
+  }
+  submit() {
+    let data = {
+      name: this.state.name,
+      email: this.state.email
+    };
+    // fetch('/create', {
+    //   method: 'POST',
+    //   // mode: 'CORS',
+    //   changeOrigin: true,
+    //   headers: {
+    //     // 'Accept': 'application/json',
+    //     // 'Access-Control-Allow-Origin': '*',
+    //     'Content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(data)
+    // }).then((result) => {
+    //   return result.json();
+    // }).then((response) => {
+    //   console.log(response);
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
 
-export default App;
+    axios.post('/create', { data }).then(res => {
+      console.log(res.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+
+    // fetch('/').then((result) => {
+    //   return result.json();
+    // }).then((response) => {
+    //   console.log(response);
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="http://localhost:4000/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <form onSubmit={this.submit}>
+          <input type="text" name="name" className="form-control" defaultValue={this.state.name} onChange={this.changeName} placeholder="Name" />
+          <input type="text" name="email" className="form-control" defaultValue={this.state.email} onChange={this.changeEmail} placeholder="Email" />
+          <input type="button" defaultValue="Submit" className="btn btn-primary" onClick={this.submit} />
+        </form>
+      </div>
+    );
+  }
+};
